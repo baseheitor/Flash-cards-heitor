b7ec3684354b457db4b865ed7e9c1b074bcd22a7
heitor base <heitor.base@escola.pr.gov.br>
2025-09-09 08:46:11 -0300

    9-9

---
+++ perguntas.js
@@ -0,0 +1,65 @@
+criaCartao(
+'categoria',
+'pergunta',
+'resposta'
+)
+
+criaCartao(
+'Geografia',
+'Qual a montanha mais alta do mundo?',
+'Monte Everest'
+)
+
+criaCartao(
+'Geografia',
+'Onde se localiza Machu Picchu?',
+'Peru'
+)
+
+criaCartao(
+'Geografia',
+'Que país tem o formato de uma bota?',
+'Itália'
+)
+
+criaCartao(
+'Geografia',
+'Qual o monumento famoso pela sua inclinação?',
+'Torre de Pisa'
+)
+
+criaCartao(
+'Historia',
+'Quem pintou Mona Lisa?',
+'Leonardo Da Vinci'
+)
+
+criaCartao(
+'Química',
+'Qual o nome popular do cloreto de sódio?',
+'Sal de cozinha'
+)
+
+criaCartao(
+'Esportes',
+'Depois do futebol, qual o esporte mais popular no Brasil?',
+'Volei'
+)
+
+criaCartao(
+'Geografia',
+'Qual a montanha mais alta do mundo?',
+'Monte Everest'
+)
+
+criaCartao(
+'Geografia',
+'Qual a montanha mais alta do mundo?',
+'Monte Everest'
+)
+
+criaCartao(
+'Geografia',
+'Qual a montanha mais alta do mundo?',
+'Monte Everest'
+)
