db1dfd6c62d2a997fcf495dc233e050c50140084
heitor base <heitor.base@escola.pr.gov.br>
2025-09-23 10:33:36 -0300

    23-09

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ let respostaEstaVisivel = false
 
 function viraCartao(){
 respostaEstaVisivel=!respostaEstaVisivel
+cartao.classList.toogle('active',respostaEstaVisivel)
 }
 
-
+cartao.addEventListener('click',viraCartao)
 container.appendChild(cartao)
 
 }
